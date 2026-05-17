a32caf6007ba24194890ac7650e78ddea5cf43a5
vDhinithya <dhinithya12@gmail.com>
2026-05-17 21:06:08 +0530

    Initial project setup

---
+++ src/main/java/com/stag/olap/OlapApplication.java
@@ -0,0 +1,11 @@
+package com.stag.olap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OlapApplication.class, args);
+    }
+}
